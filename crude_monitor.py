42821b33984671b4ae613ff84e552d16a96fda84
jeremy763 <xyh1997jeremy@gmail.com>
2021-09-26 12:32:39 -0600

    it can check the date and fetch data now

--- crude_monitor.py
+++ crude_monitor.py
@@ -1,11 +1,15 @@
+import datetime
 import sys
-
+import numpy as np
 import requests
+import pandas as pd
+import re
+import io
 import getopt
 
 URL = "https://www.crudemonitor.ca/savePHPExcel.php"
 
-acronym = "MSW"
+#acronym = "msw"
 name = "Mixed Sweet Blend"
 database = "crudes"
 
@@ -13,13 +17,13 @@ form_data = {
     "date1noscript": "",
     "date2noscript": "",
     "trendProperty": "AbsoluteDensity",
-    "acr": acronym,
+    "acr": "",
     "name": name,
     "db": database,
     "basicanalysis[]": "AbsoluteDensity",
     "options": "on",
-    "date1": "13072011",
-    "date2": "13072021",
+    "date1": "",
+    "date2": "",
     "format": "Export .CSV",
     "daterangepicker_start": "",
     "daterangepicker_end": "",
@@ -27,6 +31,16 @@ form_data = {
 
 data = requests.post(url=URL, data=form_data)
 
+def convert_date(date):
+    try:
+        datetime.datetime.strptime(date, '%Y-%m-%d')
+        date_list = list(date.split("-"))
+        date_list = date_list[::-1]
+        date_string = ''.join(date_list)
+        return date_string
+
+    except ValueError:
+        print("This is the incorrect date string format. This is the incorrect date string format. It should be YYYY-MM-DD")
 
 def get_user_input():
     crude_acronym = None
@@ -34,7 +48,6 @@ def get_user_input():
     end_date = None
     operation = None
     limit = None
-
 
     # start with the arguments at index 1 since the index 0 is the file name
     argv = sys.argv[1:]
@@ -51,19 +64,35 @@ def get_user_input():
         if opt in ["--crude_acronym"]:
             crude_acronym = arg
         elif opt in ["--start_date"]:
-            start_date = arg
+            start_date = convert_date(arg)
         elif opt in ["--end_date"]:
-            end_date = arg
+            end_date = convert_date(arg)
         elif opt in ["--operation"]:
             operation = arg
         elif opt in ["--limit"]:
             limit = arg
 
-    print("crude_acronym:",crude_acronym)
-    print('start_date: {}'.format(start_date))
-    print('end_date: {}'.format(end_date))
-    print("operation:",operation)
-    print("limit:",limit)
+    form_data["acr"] = crude_acronym
+    form_data["date1"] = start_date
+    form_data["date2"] = end_date
+
+    print("crude_acronym:", crude_acronym)
+    print("start_date:",start_date)
+    print("end_date:",end_date)
+    print("operation:", operation)
+    print("limit:", limit)
+
+    # fetch data from the website
+    data = requests.post(url=URL, data=form_data)
+    # convert it to string
+    data = io.StringIO(data.text)
+    # change it to dataframe using panda
+    df = pd.read_csv(data, sep=",")
+    # get the needed data
+    filtered_data = df.loc[df['Density (kg/m^3)'] > int(limit)]
+    filtered_data = filtered_data.rename(columns={'Density (kg/m^3)': 'Density'})
+    new = filtered_data[["Date","Density"]]
+    print(new)
 
 
 get_user_input()
